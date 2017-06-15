05c4a3f6a324a5e674568943a7a979f497bbf063
yomete <iamlese@yahoo.com>
2017-06-15 06:03:48 +0100

    changed conditionals in Home.js

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -19,7 +19,7 @@ class Chat extends Component {
         this.setState({ username: localStorage.username });
         this.pusher = new Pusher('APP_KEY', {
             authEndpoint: '/pusher/auth',
-            cluster: 'eu',
+            cluster: 'YOUR CLUSTER',
             encrypted: true
         });
         this.chatRoom = this.pusher.subscribe('private-reactchat');
